6bb012ac04b8ba1098147fd7edff4bb40502f207
Hans <hans_lin99@outlook.com>
2026-07-16 15:29:19 +0800

    update email

--- src/data/aboutme.ts
+++ src/data/aboutme.ts
@@ -25,7 +25,7 @@ export const aboutMe: AboutMe = {
   // Note that links work in the description
   description:
     "Hi, I'm in the final year of my MSc in Computer Science at UBC, where I work with Professor <a href='https://www.cs.ubc.ca/~van/'>Michiel van de Panne</a> on physics-based character animation and robotics. My research focus is on planning and control for the skilled movement of physics-based humanoid characters, building on deep reinforcement learning methods. I'm also broadly interested in embodied AI and computer graphics. My ultimate goal is to create universal intelligent agents that can replace humans for dangerous tasks (or agents which surpass human capabilities). Previously during my undergrad, I worked with Professor <a href='https://sensorimotor.cs.ubc.ca/pai/'>Dinesh K. Pai</a> on motion capture topics. <br> <br> Prior to the above, I worked in the industry as a software engineer, where I developed and maintained Web Apps with JavaScript and Python. During my leisure time, I'm active in a variety of sports and competitive FPS games such as Valorant, CS:GO and Apex Legends. Fun fact: I'm an Immortal (NA top 0.5%) player in Valorant and a Master player in Apex Legends. <br> <br> <b><u>Update:</u></b> I'll be graduating in July 2026 and am actively looking for full-time opportunities related to RL, robotics, character animation, embodied AI, or software engineering. If you are interested in my profile, please feel free to contact me via email. <br> <br> <u><b>More about me down below!</u></b> &#128516;",
-  email: "xindong@cs.ubc.ca",
+  email: "hans_lin99@outlook.com",
   // funDescription: "I am a competitive fps gamer.",
   imageUrl:
     "/images/profile.jpg",
